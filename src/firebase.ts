8d31929c35ef1e987e0a696c3cbc74c8dd2f02b9
programme0419 <kinchinbrian@gmail.com>
2025-05-15 12:57:02 -0600

    firebasefront

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,11 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
+const firebaseConfig = {
+    apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_APP_FIREBASE_MEASUREMENT_ID
+};
 
-if (!firebaseConfig) {
-    throw new Error("Firebase config is not found");
-}
-
-const app = initializeApp(JSON.parse(firebaseConfig));
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
